2712922210c097e8a9121a09456276b4c61da2aa
ubuntu <ubuntu@dd-21>
2016-12-27 19:03:37 +0530

    sample new file

--- PhpProject1/test.php
+++ PhpProject1/test.php
@@ -1,5 +1,5 @@
 <?php
-  echo "wwww";
+  echo "test sample";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
